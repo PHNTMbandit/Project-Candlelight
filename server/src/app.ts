96e8c99d7c98325038faca043188cde8edc638a3
Domenic Pittari <dom.pittari@gmail.com>
2024-02-24 23:03:33 +0900

    Added mongodb validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,10 @@
+import "dotenv/config";
 import express from "express";
+
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port:${port}`);
-});
+export default app;
