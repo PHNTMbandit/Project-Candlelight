0591d8c888ac74fe1e97cc72dfe42cf1e16b4e6d
Domenic Pittari <dom.pittari@gmail.com>
2024-02-25 00:55:45 +0900

    post, get, delete and update query functions working

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+app.use((res, req, next) => {
+  next(createHttpError(404, "Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error();
+  let errorMessage = "An unknown error occurred";
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
