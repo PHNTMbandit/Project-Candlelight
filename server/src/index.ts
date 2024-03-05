cdcf0ec521c64488b927c81727efd8af421969a9
Domenic Pittari <dom.pittari@gmail.com>
2024-03-05 21:55:27 +0900

    help

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,68 @@
-import app from "./app";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
 import env from "./util/validateEnv";
+import session from "express-session";
+import userRoutes from "./routes/user";
+import tasksRoutes from "./routes/tasks";
+import MongoStore from "connect-mongo";
+import notesRoutes from "./routes/notes";
+import createHttpError, { isHttpError } from "http-errors";
+import express, { NextFunction, Request, Response } from "express";
+import { requiresAuth } from "./middleware/auth";
 import mongoose from "mongoose";
 
+const app = express();
 const port = env.PORT;
 
-mongoose
-  .connect(env.MONGO_CONNECTION_STRING)
-  .then(() => {
-    console.log("Mongoose connected");
-    app.listen(port, () => {
-      console.log(`Server started on port:${port}`);
-    });
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+
+mongoose.connect(env.MONGO_CONNECTION_STRING);
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
   })
-  .catch(console.error);
+);
+
+app.use("/api/users", userRoutes);
+app.use("/api/notes", requiresAuth, notesRoutes);
+app.use("/api/tasks", requiresAuth, tasksRoutes);
+
+app.use((res, req, next) => {
+  next(createHttpError(404, "Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error();
+  let errorMessage = "An unknown error occurred";
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port:${port}`);
+});
+
+module.exports = app;
+
+export default app;
