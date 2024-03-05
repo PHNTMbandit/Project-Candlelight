762d504ba9736b909936dc6537f2769130657c39
Domenic Pittari <dom.pittari@gmail.com>
2024-03-05 23:22:19 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,54 +15,15 @@ import mongoose from "mongoose";
 const app = express();
 const port = env.PORT;
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
-
-mongoose.connect(env.MONGO_CONNECTION_STRING);
-
-app.use(
-  session({
-    secret: env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60 * 60 * 1000,
-      secure: true,
-      httpOnly: true,
-      sameSite: "none",
-    },
-    rolling: true,
-    store: MongoStore.create({
-      mongoUrl: env.MONGO_CONNECTION_STRING,
-    }),
-  })
-);
-
-app.use("/api/users", userRoutes);
-app.use("/api/notes", requiresAuth, notesRoutes);
-app.use("/api/tasks", requiresAuth, tasksRoutes);
-
-app.use((res, req, next) => {
-  next(createHttpError(404, "Endpoint not found"));
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-  console.error();
-  let errorMessage = "An unknown error occurred";
-  let statusCode = 500;
-  if (isHttpError(error)) {
-    statusCode = error.status;
-    errorMessage = error.message;
-  }
-  res.status(statusCode).json({ error: errorMessage });
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
-
-app.listen(port, () => {
-  console.log(`Server started on port:${port}`);
-});
-
 module.exports = app;
 
 export default app;
